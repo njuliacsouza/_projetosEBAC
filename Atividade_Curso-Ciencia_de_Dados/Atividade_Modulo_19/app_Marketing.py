604d9ff4f2367fa41058f1d4fc8412a3f31fdf3e
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2023-03-09 15:37:26 -0300

    :tada: aula de Streamlit II

--- Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_19/app_Marketing.py
+++ Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_19/app_Marketing.py
@@ -7,16 +7,16 @@ from PIL import Image
 import streamlit as st
 
 def main():
-    st.set_page_config(page_title="Bank Telemarketing Analysis",
+    st.set_page_config(page_title="Telemarketing Analysis",
                        page_icon = "img/telemarketing_icon.png",
                        layout="wide",
                        initial_sidebar_state="expanded", 
                        )
-    st.write("# Bank Telemarketing Analysis")
+    st.write("# Telemarketing Analysis")
     st.markdown("---")
     
     image = Image.open('img/bank_icon.png')
-    st.sidebar.image(image)
+    st.sidebar.image(image,clamp=True, width=200)
     
 if __name__ == '__main__':
     main()
