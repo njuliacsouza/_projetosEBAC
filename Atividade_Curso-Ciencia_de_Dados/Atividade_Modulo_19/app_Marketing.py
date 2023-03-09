35d84efdbe9f36dd7745eb5c1ca9a7e72ee9ab75
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2023-03-09 18:24:05 -0300

    :tada: aula de Streamlit II

--- Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_19/app_Marketing.py
+++ Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_19/app_Marketing.py
@@ -10,6 +10,13 @@ import plotly.graph_objects as go
 from PIL import Image
 import streamlit as st
 
+
+def multiselect_filter(relatorio, col, selecionados):
+    if 'all' in selecionados:
+        return relatorio
+    else:
+        return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
+
 def main():
     st.set_page_config(page_title="Telemarketing Analysis",
                        page_icon = "img/telemarketing_icon.png",
@@ -37,56 +44,76 @@ def main():
         st.write(f"We have {len(bank_raw)} instances")
         st.write(bank_raw, use_container_width=True)
 
-    # IDADES
-    max_age = int(bank.age.max())
-    min_age = int(bank.age.min())
-    idades = st.sidebar.slider(label='Age filter', 
-                        min_value = min_age,
-                        max_value = max_age, 
-                        value = (min_age, max_age),
-                        step = 1)
-    job = st.sidebar.multiselect(label = 'Job filter',
-                                 options = list(bank.job.unique()),
-                                 default=None
-                                    )
-    marital = st.sidebar.multiselect(label = 'Marital filter',
-                                 options = list(bank.marital.unique()),
-                                 default=None
-                                    )
-    education = st.sidebar.multiselect(label = 'Education filter',
-                                 options = list(bank.education.unique()),
-                                 default=None
-                                    )
-    default = st.sidebar.multiselect(label = 'Default filter',
-                                 options = list(bank.default.unique()),
-                                 default=None
-                                    )
-    housing = st.sidebar.multiselect(label = 'Education filter',
-                                 options = list(bank.housing.unique()),
-                                 default=None
-                                    )
-    loan = st.sidebar.multiselect(label = 'Loan filter',
-                                 options = list(bank.loan.unique()),
-                                 default=None
-                                    )
-    contact = st.sidebar.multiselect(label =  'Contact filter',
-                                 options = list(bank.contact.unique()),
-                                 default=None
-                                    )
-    month = st.sidebar.multiselect(label = 'Month filter',
-                                 options = list(bank.month.unique()),
-                                 default=None
-                                    )
+    with st.sidebar.form(key='my_form'):
+        submit_button = st.form_submit_button(label='Apply')
     
-    bank = bank[(bank['age'] >= idades[0]) & (bank['age'] <= idades[1])]
-    bank = bank.apply(lambda i: i['job'] in job if job != [] else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.marital in marital if marital else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.education in education if education else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.default in default if default else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.housing in housing if housing else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.loan in loan if loan else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.contact in contact if contact else i, axis=1).reset_index(drop=True)
-    bank = bank.apply(lambda i: i.month in month if month else i, axis=1).reset_index(drop=True)
+        # IDADES
+        max_age = int(bank.age.max())
+        min_age = int(bank.age.min())
+        idades = st.slider(label='Idade', 
+                            min_value = min_age,
+                            max_value = max_age, 
+                            value = (min_age, max_age),
+                            step = 1)
+
+
+        # PROFISSÕES
+        jobs_list = bank.job.unique().tolist()
+        jobs_list.append('all')
+        jobs_selected =  st.multiselect("Job", jobs_list, ['all'])
+
+        # ESTADO CIVIL
+        marital_list = bank.marital.unique().tolist()
+        marital_list.append('all')
+        marital_selected =  st.multiselect("Marital status", marital_list, ['all'])
+
+        # DEFAULT?
+        default_list = bank.default.unique().tolist()
+        default_list.append('all')
+        default_selected =  st.multiselect("Default", default_list, ['all'])
+
+        
+        # TEM FINANCIAMENTO IMOBILIÁRIO?
+        housing_list = bank.housing.unique().tolist()
+        housing_list.append('all')
+        housing_selected =  st.multiselect("Housing", housing_list, ['all'])
+
+        
+        # TEM EMPRÉSTIMO?
+        loan_list = bank.loan.unique().tolist()
+        loan_list.append('all')
+        loan_selected =  st.multiselect("Loan", loan_list, ['all'])
+
+        
+        # MEIO DE CONTATO?
+        contact_list = bank.contact.unique().tolist()
+        contact_list.append('all')
+        contact_selected =  st.multiselect("Contact", contact_list, ['all'])
+
+        
+        # MÊS DO CONTATO
+        month_list = bank.month.unique().tolist()
+        month_list.append('all')
+        month_selected =  st.multiselect("Contact month", month_list, ['all'])
+
+        
+        # DIA DA SEMANA
+        day_of_week_list = bank.day_of_week.unique().tolist()
+        day_of_week_list.append('all')
+        day_of_week_selected =  st.multiselect("Day of the week", day_of_week_list, ['all'])
+
+
+        bank = (bank.query("age >= @idades[0] and age <= @idades[1]")
+                    .pipe(multiselect_filter, 'job', jobs_selected)
+                    .pipe(multiselect_filter, 'marital', marital_selected)
+                    .pipe(multiselect_filter, 'default', default_selected)
+                    .pipe(multiselect_filter, 'housing', housing_selected)
+                    .pipe(multiselect_filter, 'loan', loan_selected)
+                    .pipe(multiselect_filter, 'contact', contact_selected)
+                    .pipe(multiselect_filter, 'month', month_selected)
+                    .pipe(multiselect_filter, 'day_of_week', day_of_week_selected)
+        )
+    
     
     show_filtered_dataset = st.checkbox('Show filtered dataset')
 
@@ -94,22 +121,61 @@ def main():
         st.write('### Filtered Dataset')
         st.write(f"We have {len(bank)} instances")
         st.write(bank, use_container_width=True)
+    
+    st.markdown('---')
+    
+    ## Plots
+    
+    st.write('## Distribution of numerical atributes')
+    selected_column = st.selectbox('Column', bank.select_dtypes('number').columns)
+    
+    fig = go.Figure()
+    fig.add_trace(px.histogram(bank_raw[[selected_column]]))
+    fig.add_trace(px.histogram(bank[[selected_column]]))
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
+    st.write('## Distribution of categorical atributes')
+    selected_column_cat = st.selectbox('Column', bank.select_dtypes('object').drop('y', axis=1).columns)
+    
+    fig = go.Figure()
 
-    ## PLOTS    
-    fig = make_subplots(1,2)
-
-    bank_raw_target_perc = bank_raw.y.value_counts(normalize = True).to_frame()*100
+    bank_raw_target_perc = bank_raw[selected_column_cat].value_counts(normalize = True).to_frame()*100
     bank_raw_target_perc = bank_raw_target_perc.sort_index()
     
     fig.add_trace(go.Bar(x = bank_raw_target_perc.index.values, 
-                         y = bank_raw_target_perc.y.values,
+                         y = bank_raw_target_perc[selected_column_cat].values,
                          name='Original data'
                          ))
     
-    bank_target_perc = bank.y.value_counts(normalize = True).to_frame()*100
+    bank_target_perc = bank[selected_column_cat].value_counts(normalize = True).to_frame()*100
     bank_target_perc = bank_target_perc.sort_index()
     fig.add_trace(go.Bar(x = bank_target_perc.index.values, 
-                         y = bank_target_perc.y.values,
+                         y = bank_target_perc[selected_column_cat].values,
+                         name='Filtered data'
+                         ))
+    fig.update_layout(title_text="Proportion of acceptance",
+                  showlegend=True
+                 )
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
+    st.write('## Distribution of response column')
+      
+    fig = make_subplots(1,2)
+
+    bank_raw_target_perc = bank_raw['y'].value_counts(normalize = True).to_frame()*100
+    bank_raw_target_perc = bank_raw_target_perc.sort_index()
+    
+    fig.add_trace(go.Bar(x = bank_raw_target_perc.index.values, 
+                         y = bank_raw_target_perc['y'].values,
+                         name='Original data'
+                         ))
+    
+    bank_target_perc = bank['y'].value_counts(normalize = True).to_frame()*100
+    bank_target_perc = bank_target_perc.sort_index()
+    fig.add_trace(go.Bar(x = bank_target_perc.index.values, 
+                         y = bank_target_perc['y'].values,
                          name='Filtered data'
                          ))
     fig.update_layout(title_text="Proportion of acceptance",
