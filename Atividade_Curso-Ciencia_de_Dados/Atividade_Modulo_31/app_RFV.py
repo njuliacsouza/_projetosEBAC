82bb265f85ca7486aeb849c7a7aae26fe6155433
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2023-03-14 21:42:02 -0300

    :tada: tarefa aula de RFV - streamlit

--- Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_31/app_RFV.py
+++ Atividade_Curso-Ciencia_de_Dados/Atividade_Modulo_31/app_RFV.py
@@ -61,19 +61,15 @@ def main():
 
     st.write("""RFV significa recência, frequência, valor e é utilizado para segmentação de clientes baseado no comportamento 
     de compras dos clientes e agrupa eles em clusters parecidos. Utilizando esse tipo de agrupamento podemos realizar 
-    ações de marketing e CRM melhores direcionadas, ajudando assim na personalização do conteúdo e até a retenção de clientes.
-
-    Para cada cliente é preciso calcular cada uma das componentes abaixo:
-
-    - Recência (R): Quantidade de dias desde a última compra.
-    - Frequência (F): Quantidade total de compras no período.
-    - Valor (V): Total de dinheiro gasto nas compras do período.
-
-    E é isso que iremos fazer abaixo.
-    """)
+    ações de marketing e CRM melhores direcionadas, ajudando assim na personalização do conteúdo e até a retenção de clientes.""")
+    st.write("Para cada cliente é preciso calcular cada uma das componentes abaixo:")
+    st.write("- Recência (R): Quantidade de dias desde a última compra.")
+    st.write("- Frequência (F): Quantidade total de compras no período.")
+    st.write("- Valor (V): Total de dinheiro gasto nas compras do período.")
+    st.write("E é isso que iremos fazer abaixo. ")
     st.markdown("---")
     
-    image = Image.open("bank-icon.png")
+    image = Image.open("bank_icon.png")
     st.sidebar.image(image)
 
     st.sidebar.write("## Suba o arquivo")
